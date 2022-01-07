dad3af62857236c047a92c54f39adb87f558d2d3
devilxcv2 <75221977+devilxcv2@users.noreply.github.com>
2022-01-07 16:47:26 +0100

    Add files via upload

---
+++ srally.sh
@@ -0,0 +1,2 @@
+cd /home/aresuser/Downloads/model3/bin
+./supermodel -legacy3d -sound-volume=50 -music-volume=80 -no-vsync -no-throttle -no-dsb -res=640,480 -fullscreen /home/aresuser/ARES/roms/sm3/srally2.zip
